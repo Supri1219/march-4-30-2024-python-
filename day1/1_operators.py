c821ac9cf7ee3111bc6144993ab88338381b0ea1
Supri1219 <supriyapokharel99@gmail.com>
2024-03-11 17:12:47 +0545

    commit message

---
+++ day1/1_operators.py
@@ -0,0 +1,46 @@
+# Like in any other programming language python also has it's set of operators
+# Operators in python are:
+# 1. Arithmetic Operators
+# 2. Logical Operators
+# 3. Relational Operators
+# 4. Assignment Operators
+# 5. Membership Operator
+# 6. Identity Operator
+
+
+# 1. Arithmetic Operators
+
+# Addition(+)
+a = 1  # a is a variable; = is an operator and 1 is a data of integer type
+b = 2
+print(a + b)  # print() is a python built-in function to view the results in a consoleb = 
+# Subtraction
+a = 5
+b = 3
+print(a -b)
+# Division (/)
+# Multiplication (*)
+# Exponent (**)
+# Floor Division (//)
+# Modulus Operator (%)
+
+# 2. Logical Operators
+# and, or, not
+print (True and False)
+print (not False)
+print (not True)
+# 3. Relational Operators
+# >, <, >=,<=, !=, ==
+# Result is in boolean (True/False)
+# 4. Assignment Operators
+# =, +=, -=, *=
+a=1
+a+=1
+print (a)
+# 5. Membership Operators
+# in, not in
+# result in boolean
+# checks whether an element is a member of iterable
+# Iterable means sequence of data
+Vowels = ("a", "e", "i", "o", "u")
+print ("b" in Vowels)
---
+++ day1/3_syntax.py
@@ -0,0 +1,25 @@
+# syntax is like grammar of programming language
+# is we mess up with syntax then the program cant be compiled
+# identation is key factir to maintain coed blocks in python. these are equivalent to curly-braces in most other languages
+# 1. identation means 1 tab or 4 spaces in python
+#2. 
+# Python statements
+# Each line of a python program is an statement
+# But, we can also write multiple statements in a same line
+
+a = 1; b= 2
+
+# We can also write a single statement in multiple lines
+a = "Hello World. I am " \
+    "learning Python"
+
+# Triple quoted strings or docstring
+a = """
+Hello World.
+I am learning Python
+"""
+
+a = '''
+Hello World.
+I am learning Python
+'''
